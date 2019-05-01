6a1ffe36e96b53d9fe8f7502f96b01b41a1b9e0b
Xiao Yu Wnag <40452784+Ohpaipai@users.noreply.github.com>
2019-05-02 01:48:38 +0800

    Delete VectorAndMatrix.h

--- VectorAndMatrix.h
+++
@@ -1,8 +0,0 @@
-#include"Matrix.h"
-
-Matrix operator*( VectorSpace &vec, Matrix&_matrix);
-Matrix operator*(Matrix&_matrix, VectorSpace &vec);
-Matrix operator+(VectorSpace &vec, Matrix&_matrix);
-Matrix operator+(Matrix&_matrix, VectorSpace &vec);
-Matrix operator-(Matrix&_matrix, VectorSpace &vec);
-Matrix operator-(VectorSpace &vec, Matrix&_matrix);
